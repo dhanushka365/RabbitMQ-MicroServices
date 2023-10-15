447139707c52047b76142e762e518f87914a65a1
dhnaushka365 <pasindudhanushka365@gmail.com>
2023-10-15 13:05:58 +0530

    Initial Commit of Infrastructure Dependency Project

---
+++ RabbitMQ-Microservices.Infrastructure.IoC/DependencyContainer.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using RabbitMQ_MicroServices.Domain.Core.Bus;
+using RabbitMQ_MicroServices.Infrastructure.Bus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RabbitMQ_Microservices.Infrastructure.IoC
+{
+    public class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            //Domain Bus
+            services.AddTransient<IEventBus, RabbitMQBus>();
+            //Domain Events
+            //services.AddTransient<IEventHandler<TransferCreatedEvent>, TransferEventHandler>();
+            ////Application Services
+            //services.AddTransient<IAccountService, AccountService>();
+            ////Data
+            //services.AddTransient<IAccountRepository, AccountRepository>();
+            //services.AddTransient<BankingDbContext>();
+        }
+    }
+}
