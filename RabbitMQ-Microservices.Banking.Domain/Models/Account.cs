83f31ff8c490e86180e8c8f350a7e2d9aabde582
dhnaushka365 <pasindudhanushka365@gmail.com>
2023-10-16 12:34:46 +0530

    Neccesary libraries added to the Banking.Data Layer

---
+++ RabbitMQ-Microservices.Banking.Domain/Models/Account.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RabbitMQ_MicroServices.Banking.Domain.Models
+{
+    public class Account
+    {
+        public int Id { get; set; }
+        public string AccountType { get; set; }
+        public decimal Balance { get; set; }
+
+    }
+}
