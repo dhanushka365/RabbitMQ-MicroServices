dbfdc90ebf6830be4abe00e0112fe1d9be71727c
dhnaushka365 <pasindudhanushka365@gmail.com>
2023-10-17 07:15:26 +0530

    works commited

--- RabbitMQ-Microservices.Banking.Api/Program.cs
+++ RabbitMQ-Microservices.Banking.Api/Program.cs
@@ -1,59 +1,16 @@
-using MediatR;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
-using RabbitMQ_Microservices.Infrastructure.IoC;
-using RabbitMQ_MicroServices.Banking.Application.Interfaces;
-using RabbitMQ_MicroServices.Banking.Application.Services;
-using RabbitMQ_MicroServices.Banking.Data.Context;
-using System.Reflection;
+﻿using Microsoft.AspNetCore;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-
-builder.Services.AddControllers();
-builder.Services.AddMvc();
-
-builder.Services.AddDbContext<BankingDbContext>(options =>
-options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-//------------------------------------------------------------------------------------------------------------------------
-//
-var services = new ServiceCollection();// Create a service collection
-DependencyContainer.RegisterServices(services);// Register your services using the DependencyContainer                        
-//var serviceProvider = services.BuildServiceProvider(); // Build the service provider
-//----------------------------------------------------------------------------------------------------------------------
-
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(c=>
+namespace RabbitMQ_MicroServices.Banking.Api
 {
-    c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "Banking Microservice", Version = "v1" });
-});
-//builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
-//builder.Services.AddMediatR(typeof(Program).Assembly, typeof(IAccountService).Assembly);
-//builder.Services.AddScoped<IAccountService, AccountService>();
-
-//builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblyContaining(typeof(AccountService)));
-services.AddMediatR(config => config.RegisterServicesFromAssemblyContaining<Program>());
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI( c=>
+    public class Program
     {
-      c.SwaggerEndpoint("/swagger/v1/swagger.json", "Banking Microservice v1");
-    });
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
-
---
+++ RabbitMQ-Microservices.Banking.Api/Startup.cs
@@ -0,0 +1,75 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using RabbitMQ_Microservices.Infrastructure.IoC;
+using RabbitMQ_MicroServices.Banking.Data.Context;
+
+namespace RabbitMQ_MicroServices.Banking.Api
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<BankingDbContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+            }
+            );
+
+            services.AddMvc();
+
+            services.AddSwaggerGen(c =>
+            {
+                //c.SwaggerDoc("v1", new Info { title = "Banking Microservice", version = "v1" });
+            });
+
+            //services.AddMediatR(typeof(Startup));
+            services.AddMediatR(config => config.RegisterServicesFromAssemblyContaining<Startup>());
+
+            RegisterServices(services);
+        }
+
+        private void RegisterServices(IServiceCollection services)
+        {
+            DependencyContainer.RegisterServices(services);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+#pragma warning disable CS0618 // Type or member is obsolete
+        public void Configure(IApplicationBuilder app, Microsoft.AspNetCore.Hosting.IHostingEnvironment env)
+#pragma warning restore CS0618 // Type or member is obsolete
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Banking Microservice V1");
+            });
+
+            app.UseRouting(); // Add this line for Endpoint Routing
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers(); // This replaces UseMvc for routing
+            });
+        }
+    }
+}
